d9cef174ac2dcb1cdacc9d1785a776b3e953c5eb
austrargentina <dbracher@student.tgm.ac.at>
2015-03-09 12:04:54 +0100

    PyGame und PyOpenGL implementiert
    Wuerfel anzeigen

--- Sources/MVC/PlanetenScreen.py
+++ Sources/MVC/PlanetenScreen.py
@@ -1,20 +1,40 @@
 __author__ = 'Daniel'
 
+#Import der Interfaces
 from MVC.AbstractMVC import *
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
 
-import sys
+#Import der relevanten Klassen fuer Pygame und PyOpenGL
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
-class ComputeEventsPlanets(QWidget):
 
-    def __init__(self, parent = None):
-        super().__init__(parent)
-        self.myForm = Ui_SolarSystem()
-        self.myForm.setupUi(self)
+class ComputeEventsPlanets(IComputeEvents):
+
+    screen = None
+
+    def __init__(self):
+
+        #Pygame initialisieren
+        pygame.init()
+        #Fenster offnen
+        screen = DrawScreenPlanets()
+
+        while True:
+            #Keyboard-Events abfragen
+            self.computeKeyboardEvents()
+            glRotatef(1,1,0,0)
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) #clear frame !IMPORTANT!
+            Cube()
+            pygame.display.flip()
+            pygame.time.wait(10)
 
     def computeKeyboardEvents(self):
-        pass
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
 
     def computeMouseEvents(self):
         pass
@@ -22,35 +42,21 @@ class ComputeEventsPlanets(QWidget):
 class DrawScreenPlanets(IDrawScreen):
 
     def __init__(self):
-        pass
+        #Displaygroesse einstellen
+        display = (800, 600)
+        pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+        #Perspektive aendern
+        gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+        #Ursprung verschieben
+        glTranslatef(0.0,0.0,-5)
 
     def drawButtons(self):
         pass
 
     def drawContent(self):
         pass
-
-class Ui_SolarSystem(object):
-    def setupUi(self, SolarSystem):
-        SolarSystem.setObjectName("SolarSystem")
-        SolarSystem.resize(800, 600)
-        SolarSystem.setMinimumSize(QtCore.QSize(800, 600))
-        SolarSystem.setMaximumSize(QtCore.QSize(800, 600))
-        SolarSystem.setAutoFillBackground(False)
-        self.gridLayoutWidget = QtWidgets.QWidget(SolarSystem)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 801, 601))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.Content = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.Content.setContentsMargins(0, 0, 0, 0)
-        self.Content.setObjectName("Content")
-
-        self.retranslateUi(SolarSystem)
-        QtCore.QMetaObject.connectSlotsByName(SolarSystem)
-
-    def retranslateUi(self, SolarSystem):
-        _translate = QtCore.QCoreApplication.translate
-        SolarSystem.setWindowTitle(_translate("SolarSystem", "SolarSystem"))
-
 
 class ScreenPlanets(IScreen):
 
@@ -59,9 +65,3 @@ class ScreenPlanets(IScreen):
 
     def getObjects(self):
         pass
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    c = ComputeEventsPlanets()
-    c.show()
-    sys.exit(app.exec_())
